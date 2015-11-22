8b18f9f9e8b07236e9ce4aba918210f6c1a7fcc4
diegopacheco <diego.pacheco.it@gmail.com>
2015-11-22 11:27:48 -0800

    .
    .

--- apache-flink-scala-stream-fun/build.sbt
+++ apache-flink-scala-stream-fun/build.sbt
@@ -2,9 +2,9 @@ name := "apache-flink-scala-stream-fun"
 
 version := "1.0"
 
-scalaVersion := "2.10.5"
+scalaVersion := "2.11.7"
 
-scalaVersion in ThisBuild := "2.10.5"
+scalaVersion in ThisBuild := "2.11.7"
 
 resolvers += "Akka Repo" at "http://repo.akka.io/releases" 
 resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
@@ -13,24 +13,15 @@ resolvers += "Sonatype snapshots" at "https://oss.sonatype.org/content/repositor
 resolvers += "Twitter Repo" at "http://maven.twttr.com/"
 resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
+libraryDependencies += "org.apache.flink" % "flink-streaming-scala_2.11" % "0.10.0"
+
+//libraryDependencies += "org.apache.flink" % "flink-scala" % "0.10.0"
+//libraryDependencies += "org.apache.flink" % "flink-clients" % "0.10.0"
+//libraryDependencies += "org.apache.flink" % "flink-streaming-core" % "0.9.1"
 //libraryDependencies += "org.apache.flink" % "flink-core" % "0.10.0"
-libraryDependencies += "org.apache.flink" % "flink-scala" % "0.10.0"
-libraryDependencies += "org.apache.flink" % "flink-clients" % "0.10.0"
 //libraryDependencies += "org.apache.flink" % "flink-java8" % "0.10.0"
 //libraryDependencies += "org.apache.flink" % "flink-runtime" % "0.10.0"
 //libraryDependencies += "org.apache.flink" % "flink-java8" % "0.10.0"
-//libraryDependencies += "org.apache.flink" % "flink-streaming-core" % "0.9.1"
+
 
 EclipseKeys.withSource := true
-
-lazy val commonSettings = Seq(
-  version      := "1.0-SNAPSHOT",
-  organization := "com.github.diegopacheco.sandbox.stream.flink",
-  scalaVersion := "2.10.5"
-)
-
-lazy val app = (project in file("app")).
-  settings(commonSettings: _*).
-  settings(
-    // your settings here
-  )
---
+++ apache-flink-scala-stream-fun/src/main/scala/com/github/diegopacheco/sandbox/stream/flink/stream/fun/CarStream.scala
@@ -0,0 +1,59 @@
+package com.github.diegopacheco.sandbox.stream.flink.stream.fun
+
+import java.util.concurrent.TimeUnit
+
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.windowing.delta.DeltaFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows
+import org.apache.flink.streaming.api.windowing.evictors.TimeEvictor
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.triggers.DeltaTrigger
+
+import scala.Stream._
+import scala.math._
+import scala.language.postfixOps
+import scala.util.Random
+
+class CarStream extends App {
+    
+   case class CarEvent(carId: Int, speed: Int, distance: Double, time: Long)
+   
+   def genCarStream(): Stream[CarEvent] = {
+      
+      def nextSpeed(carEvent:CarEvent):CarEvent = {
+        val next =
+          if (Random.nextBoolean) min(100, carEvent.speed + 5) else max(0, carEvent.speed - 5)
+        CarEvent(carEvent.carId, next, carEvent.distance + next/3.6d,System.currentTimeMillis)
+      }
+      def carStream(speeds : Stream[CarEvent]):Stream[CarEvent] = {
+        Thread.sleep(1000)
+        speeds.append(carStream(speeds.map(nextSpeed)))
+      }
+      
+      carStream(range(0, numOfCars).map(CarEvent(_,50,0,System.currentTimeMillis())))
+  }
+   
+   val numOfCars = 2
+   val evictionSec = 10
+   val triggerMeters = 50d
+   
+   val env = StreamExecutionEnvironment.getExecutionEnvironment
+   env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+   env.setParallelism(1)
+    
+   val cars = env.fromCollection(genCarStream())
+   
+   val topSeed = cars
+      .assignAscendingTimestamps( _.time )
+      .keyBy("carId")
+      .window(GlobalWindows.create)
+      .evictor(TimeEvictor.of(Time.of(evictionSec * 1000, TimeUnit.MILLISECONDS)))
+      .trigger(DeltaTrigger.of(triggerMeters, new DeltaFunction[CarEvent] {
+        def getDelta(oldSp: CarEvent, newSp: CarEvent): Double = newSp.distance - oldSp.distance
+      }))
+      .maxBy("speed")
+
+    topSeed.print
+  
+}
