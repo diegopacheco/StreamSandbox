4324af0d909ecc70935fb59f4bb8c8cf11f80545
diegopacheco <diego.pacheco.it@gmail.com>
2015-11-22 21:49:45 -0800

    .
    .

--- apache-spark-streaming-scala-kafka/src/main/scala/com/github/diegopacheco/sandbox/stream/spark/streaming/kafka/fun/KafkaSparkWordCount.scala
+++ apache-spark-streaming-scala-kafka/src/main/scala/com/github/diegopacheco/sandbox/stream/spark/streaming/kafka/fun/KafkaSparkWordCount.scala
@@ -1,7 +1,25 @@
 package com.github.diegopacheco.sandbox.stream.spark.streaming.kafka.fun
 
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka._
+import org.apache.spark.SparkConf
+
 object KafkaSparkWordCount extends App {
   
-  
-  
+    val Array(zkQuorum, group, topics, numThreads) = Array[String]("localhost","my-consumer-group","test","1")
+    val sparkConf = new SparkConf().setAppName("KafkaSparkStreamWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    ssc.checkpoint("checkpoint")
+
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
+    
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L))
+      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+    
 }
---
+++ apache-spark-streaming-scala-kafka/src/main/scala/com/github/diegopacheco/sandbox/stream/spark/streaming/kafka/fun/KafkaWordCountProducer .scala
@@ -0,0 +1,27 @@
+package com.github.diegopacheco.sandbox.stream.spark.streaming.kafka.fun
+
+import java.util.HashMap
+
+import org.apache.kafka.clients.producer.{ProducerConfig, KafkaProducer, ProducerRecord}
+
+object KafkaWordCountProducer  extends App {
+    
+    val Array(brokers, topic, messagesPerSec, wordsPerMessage) = Array[String]("localhost","test","10","100")
+
+    val props = new HashMap[String, Object]()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](props)
+
+    while(true) {
+      (1 to messagesPerSec.toInt).foreach { messageNum =>
+          val str = (1 to wordsPerMessage.toInt).map(x => scala.util.Random.nextInt(10).toString).mkString(" ")
+          val message = new ProducerRecord[String, String](topic, null, str)
+          producer.send(message)
+      }
+      Thread.sleep(1000)
+    }
+  
+}
